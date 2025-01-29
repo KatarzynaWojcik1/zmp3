b538c336e7a49eaa137a028aa06de08a85c4ae53
KatarzynaWojcik1 <u60613@student.wat.edu.pl>
2025-01-29 20:21:25 +0100

    correct commentaries

--- adapter/Adapter/adapter.cpp
+++ adapter/Adapter/adapter.cpp
@@ -1,43 +1,24 @@
 #include "adapter.h"
 
-void RobotAdapter::cartesiantopolar(double x, double y){
-       using namespace std;
-        
-       double deg;
-       double dist;
-       
-       deg = atan(y/x)*180/M_PI;
-       dist = sqrt(pow(x,2)+pow(y,2));
-       
-       std::cout<< fixed << setprecision(0) << dist << "exp(" <<deg<<")"<<std::endl;
-       
-    }
+void RobotAdapter::cartesiantopolar(double x, double y) {
+    using namespace std;
+
+    // Declare variables for polar coordinates
+    double deg;  // Angle in degrees
+    double dist; // Distance from origin
+
+    // Calculate the angle in degrees
+    deg = atan(y / x) * 180 / M_PI;
+
+    // Calculate the distance from the origin
+    dist = sqrt(pow(x, 2) + pow(y, 2));
+
+    // Print the polar coordinates in the format "distance exp(angle)" with 0 decimal places
+    std::cout << fixed << setprecision(0) << dist << "exp(" << deg << ")" << std::endl;
+}
 
 // Adapter class to make the OldRobot compatible with NewRobot
 void RobotAdapter::goTo(double x, double y){
     std::cout<< "Stary robot poszedl do: ";
     cartesiantopolar(x,y);
 };
-
-
-
-
-//  : public OldRobot {
-// public:
-//     void cartesiantopolar(double x, double y){
-//        using namespace std;
-        
-//        double deg;
-//        double dist;
-       
-//        deg = atan(y/x)*180/M_PI;
-//        dist = sqrt(pow(x,2)+pow(y,2));
-       
-//        std::cout<< fixed << setprecision(0) << dist << "exp(" <<deg<<")"<<std::endl;
-       
-//     }
-//      void goTo(double x, double y) override{
-//         std::cout<< "Stary robot poszedl do: ";
-//         cartesiantopolar(x,y);
-//     }
-// };
--- adapter/OldRobot/oldrobot.cpp
+++ adapter/OldRobot/oldrobot.cpp
@@ -1,8 +1,5 @@
-
-
 #include "oldrobot.h"
 
-// Old robot class
 void OldRobot::goTo(double x, double y){
         std::cout<< "Stary robot poszedl do: " << x << ", " << y << std::endl;
 };
